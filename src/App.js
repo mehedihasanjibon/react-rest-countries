80915a2ebf9d49a9a50f2d8cfd72836744be06f5
Mehedi Hasan <mhjibon.info@gmail.com>
2023-06-18 02:18:54 +0600

    setup in footer and map

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
 import Countries from './components/Countries/Countries';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       {/* <LoadCountries></LoadCountries> */}
       <Countries></Countries>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p><small> &copy; Copyright Protected </small></p>
+            <iframe width="98%" height="100%" src="https://maps.google.com/maps?q=Lakshmipur&t=&z=12&ie=UTF8&iwloc=&output=embed"></iframe>
+        </div>
+    );
+};
+
+export default Footer;
