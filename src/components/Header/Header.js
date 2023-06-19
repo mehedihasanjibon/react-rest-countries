7401b800bc98f3006a1b5d82609b6e8ff8b41839
Mehedi Hasan <mhjibon.info@gmail.com>
2023-06-19 23:47:56 +0600

    header section added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import './Header.css'
 
 const Header = () => {
     return (
         <div>
             <h1> Welcome to my Rest Countries </h1>
-            <nav className='menu'>
+            <nav className='menubar'>
                 <a href="/home"> Home </a>
                 <a href="/countries"> Countries </a>
+                <a href="/menu"> Menu </a>
                 <a href="/about"> About </a>
             </nav>
         </div>
