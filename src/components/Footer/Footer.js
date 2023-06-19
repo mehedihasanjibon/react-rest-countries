95745808b48057dce205303575d56805c516c07e
Mehedi Hasan <mhjibon.info@gmail.com>
2023-06-19 23:57:58 +0600

    footer section added

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p> &copy; Copyright Protected </p>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29393.707622891277!2d90.81496216709594!3d22.94236474174968!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3754c164d7252c0f%3A0x2ff6bb814b6f2a69!2sLakshmipur!5e0!3m2!1sen!2sbd!4v1687197261697!5m2!1sen!2sbd" width="100%" height="100%"></iframe>
+        </div>
+    );
+};
+
+export default Footer; 
